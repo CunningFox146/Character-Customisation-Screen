7e8b0103a92a3fd7a1d07ddcdb39799d89e2ff17
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-10-11 19:34:34 +0300

    Added tabs switching

---
+++ Assets/_Core/Scripts/UI/Skins/CustomisationPanel.cs
@@ -0,0 +1,16 @@
+﻿using DonutLab.SkinSources;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DonutLab.UI.Skins
+{
+    public class CustomisationPanel : MonoBehaviour
+    {
+        [SerializeField] private SkinSourceCharacter _sourceCharacter;
+        [SerializeField] private SkinSourceStand _sourceStand;
+        [SerializeField] private SkinsGrid _skinsGrid;
+
+        public void ShowCharacterSkins() => _skinsGrid.SetSource(_sourceCharacter);
+        public void ShowStandSkins() => _skinsGrid.SetSource(_sourceStand);
+    }
+}
--- Assets/_Core/Scripts/UI/Skins/SkinsGrid.cs
+++ Assets/_Core/Scripts/UI/Skins/SkinsGrid.cs
@@ -1,6 +1,4 @@
-﻿using DonutLab.Skins;
-using DonutLab.SkinSources;
-using UnityEngine;
+﻿using UnityEngine;
 using VirtualList;
 
 namespace DonutLab.UI.Skins
@@ -8,13 +6,7 @@ namespace DonutLab.UI.Skins
     public class SkinsGrid : MonoBehaviour
     {
         [SerializeField] private VirtualGridList _gridList;
-        [SerializeField] private SkinSourceCharacter _characterSkinData;
 
         public void SetSource(IListSource source) => _gridList.SetSource(source);
-
-        private void Awake()
-        {
-            SetSource(_characterSkinData);
-        }
     }
 }
